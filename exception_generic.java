86df0c31a25e9fe73b98716c75112c4e3211b76e
paathak <38093288+paathak@users.noreply.github.com>
2020-12-31 13:35:31 +0530

    Create exception_generic.java

---
+++ exception_generic.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Main.doThrowGeneric(new IOException());
+		//Main.doThrowGeneric(new RuntimeException());
+		//Main.<Exception>doThrowGeneric(new RuntimeException()); compiler has clarity now 
+		//Main.<Exception>doThrowGeneric(new IOException()); compiler has clarity now
+		
+	}
+	//the compiler is confused here as E can be any subclass of exception checked or unchecked
+	//if compiler mandatorily gives compile error then what in case if it was runtime Exception
+	public static <E extends Exception> void doThrowGeneric(Exception e) throws E {
+	    throw (E)e;
+	    
+	}
+	
+}
