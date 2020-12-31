eb605fbe742684682025c1682c18ae293cfefe55
unknown <javafullstack08@iiht.tech>
2020-12-31 22:56:12 +0530

    update_Exception_generic_renamed_ExceptionUnderHood

--- src/main/java/com/training/javatr/ExceptionUnderHood.java
+++ src/main/java/com/training/javatr/ExceptionUnderHood.java
@@ -1,17 +1,23 @@
+package com.training.javatr;
+
+
 import java.io.*;
-public class Main
+
+public class ExceptionUnderHood
 {
 	public static void main(String[] args) {
-		Main.doThrowGeneric(new IOException()); //java will throw checked exception !!!!
+		ExceptionUnderHood.doThrowGeneric(new IOException()); //java will throw checked exception !!!!
 		//Main.doThrowGeneric(new RuntimeException());
 		
 		// In above two cases java compiler took side of Unchecked exceptions, reason explained on line 17, 18 
 		
-		//Main.<Exception>doThrowGeneric(new RuntimeException()); compiler has clarity now 
-		//Main.<Exception>doThrowGeneric(new IOException()); compiler has clarity now
+		//ExceptionUnderHood.<Exception>doThrowGeneric(new RuntimeException()); compiler has clarity now 
+		//ExceptionUnderHood.<Exception>doThrowGeneric(new IOException()); compiler has clarity now
 		
-		//Main.<RuntimeException>doThrowGeneric(new IOException()); //compiler has clarity now & this will work too and won't fail while casting!!!!
-		
+		//ExceptionUnderHood.<RuntimeException>doThrowGeneric(new IOException()); //compiler has clarity now & this will work too and won't fail while casting!!!!
+		Exception e = new IOException();
+		IOException i = (IOException)e;
+		i.getCause();
 	}
 	//the compiler is confused here as E can be any subclass of exception checked or unchecked
 	//if compiler mandatorily gives compile error then what in case if it was runtime Exception , compiler just took side of unchecked here!! (obviously
