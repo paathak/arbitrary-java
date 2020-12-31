872857415b3e07f4ff91ca8d185add39e17a9a1e
paathak <38093288+paathak@users.noreply.github.com>
2020-12-31 15:02:44 +0530

    Update exception_generic.java

--- exception_generic.java
+++ exception_generic.java
@@ -14,7 +14,7 @@ public class Main
 		
 	}
 	//the compiler is confused here as E can be any subclass of exception checked or unchecked
-	//if compiler mandatorily gives compile error then what in case if it was runtime Exception , compiler just took side of unchecked here!!
+	//if compiler mandatorily gives compile error then what in case if it was runtime Exception , compiler just took side of unchecked here!! (obviously because to support existing code before generics that might not have handled an unchecked exception, one that would have handled has no impact in this case)
 	public static <E extends Exception> void doThrowGeneric(Exception e) throws E {
 	    throw (E)e;
 	    
